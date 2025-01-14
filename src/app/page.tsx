5d3c6329f75a0d8852b73176ea1a3c02b8fb91d2
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-01-14 13:35:41 +0530

    chore: update tailwind config with design tokens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main>
+    <main className="bg-gradient-text">
       <div>Hello world!</div>
     </main>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,68 @@ import type { Config } from "tailwindcss"
 
 export default {
   content: ["./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: {},
+  theme: {
+    screens: {
+      md: "48rem",
+      lg: "90rem", // Change later after testing
+    },
+
+    fontFamily: {
+      "martian-mono": ["Martian-Mono", "monospace"],
+      inter: ["Inter", "sans-serif"],
+    },
+
+    fontSize: {
+      sm: "0.875rem",
+      md: "1.125rem",
+      lg: "1.25rem",
+      xl: "1.5rem",
+      "2xl": "2.125rem",
+      "3xl": "2.375rem",
+      "4xl": "3.125rem",
+      "5xl": "3.875rem",
+    },
+
+    fontWeight: {
+      normal: "400",
+      semibold: "600",
+      bold: "700",
+    },
+
+    lineHeight: {
+      tight: "1.1",
+      tighter: "1.2",
+      snug: "1.3",
+      normal: "1.4",
+    },
+
+    letterSpacing: {
+      tighter: "-0.125rem",
+      tight: "-0.0625rem",
+      normal: "-0.03125rem",
+    },
+
+    colors: {
+      neutral: {
+        0: "hsl(0, 0%, 100%)",
+        100: "hsl(17, 41%, 97%)",
+        200: " hsl(17, 12%, 89%)",
+        700: "hsl(195, 23%, 28%)",
+        900: "hsl(194, 78%, 11%)",
+      },
+
+      salmon: {
+        50: "hsl(22, 100%, 97%)",
+        100: "hsl(22, 100%, 91%)",
+        500: "hsl(22, 99%, 72%)",
+      },
+    },
+
+    backgroundImage: {
+      gradient: "linear-gradient(90deg, #ffe2d1 0%, #fff5ef 100%)",
+      "gradient-text":
+        "linear-gradient(107deg, #ff9a60 -11.37%, #062630 61.84%)",
+    },
+  },
   plugins: [],
 } satisfies Config
